3a7a65ce5348417977f07d98dc5470f944e35f06
David Wen <dstwen@gmail.com>
2014-09-28 15:52:30 -0700

    return AB string result from guess

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,14 @@
-angular.module('starter.controllers', [])
+var app = angular.module('starter.controllers', []);
 
-.controller('PlayCtrl', function($scope, Answer) {
+app.controller('PlayCtrl', function($scope, Answer) {
   $scope.checkAnswer = function(attempt) {
-    var answer = Answer
-    console.log(answer == attempt);
+    console.log(Answer.check(attempt));
   }
-})
+});
 
-.controller('SettingsCtrl', function($scope) {
+app.controller('SettingsCtrl', function($scope) {
   $scope.user = {username: "Dawenster"}
 })
 
-.controller('HistoryCtrl', function($scope) {
+app.controller('HistoryCtrl', function($scope) {
 });
--- www/js/services.js
+++ www/js/services.js
@@ -9,6 +9,43 @@ app.factory('Answer', function() {
   // Some fake testing data
   // var answer = Math.floor(Math.random()*9000) + 1000;
   var answer = 1234;
+
+  var Answer = {};
+
+  Answer.check = function(attempt) {
+    if (answer == attempt) {
+      return "Congrats!";
+    } else {
+      return provideFeedback(answer, attempt);
+    }
+  }
   
-  return answer;
+  var provideFeedback = function(answer, attempt) {
+    var feedback = "";
+
+    splitAnswers = answer.toString().split("");
+    splitAttempt = attempt.split("");
+
+    for (var i = 0; i < splitAttempt.length; i++) {
+      feedback += determineLetter(splitAttempt[i], splitAnswers, i);
+    };
+
+    return feedback;
+  }
+
+  var determineLetter = function(letter, splitAnswers, letterIndex) {
+    var foundIndex = splitAnswers.indexOf(letter)
+
+    if (foundIndex > -1) {
+      if (foundIndex == letterIndex) {
+        return "A";
+      } else {
+        return "B";
+      }
+    } else {
+      return "";
+    }
+  }
+
+  return Answer
 });
