40a86714263f8861580306adf9099e36efc02f80
David Wen <dstwen@gmail.com>
2014-09-28 21:45:13 -0700

    fix bug where answer constantly refreshes

--- www/js/services.js
+++ www/js/services.js
@@ -4,19 +4,17 @@ var app = angular.module('starter.services', []);
  * A simple example service that returns some data.
  */
 app.factory("Answer", function() {
+  var Answer = {};
+  var ans = Math.floor(Math.random()*9000) + 1000;
+
   // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  // var answer = Math.floor(Math.random()*9000) + 1000;
-  var answer = 1234;
-
-  var Answer = {};
-
   Answer.check = function(attempt) {
-    if (answer == attempt) {
+    console.log(ans);
+    if (ans == attempt) {
       return "You win!";
     } else {
-      return provideFeedback(answer, attempt);
+      return provideFeedback(ans, attempt);
     }
   }
   
